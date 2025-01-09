84443b6cc8ddc2aa054f844bef1e437341cfeb37
MatthiasAmelinckx <matthias.amelinckx@correo.unimet.edu.ve>
2025-01-09 14:34:42 -0400

    Update holausuario.py
    se le añadio que diga el saludo segun la hora del dia

--- holausuario.py
+++ holausuario.py
@@ -1,2 +1,9 @@
+from datetime import datetime
 nombre = input ("¿Cuál es tu nombre, usuario? ")
-print ("Hola", nombre,"Bienvenido a este curso de algoritmos y programación.")
+if current_hour < 12:
+  saludo = "Buenos días "
+elif current_hour < 18:
+  saludo = "Buenas tardes "
+else:
+  saludo = "Buenas noches "
+print (f" {saludo}, {nombre}! Bienvenido a este curso de algoritmos y programación.")
